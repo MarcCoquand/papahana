c1e5a999f6c3e02b06e2493a07a3967d159075ab
Martin Bååth <martinbaath@client225-56.wireless.umu.se>
2017-05-01 21:33:51 +0200

    Add video background for loginWindow component

--- papahana/imports/ui/components/LoginWindow.js
+++ papahana/imports/ui/components/LoginWindow.js
@@ -8,7 +8,13 @@ export default class LoginWindow extends Component {
   render() {
     if (!this.props.user) {
       return (
-          <div>
+          <div className="loginWindowDiv">
+            <video autoPlay loop className="video-background" muted>
+              <source src=
+              "https://player.vimeo.com/external/158148793.hd.mp4?s=8e8741dbee251d5c35a759718d4b0976fbf38b6f&profile_id=119&oauth2_token_id=57447761"
+              type="video/mp4">
+              </source>
+            </video>
             <h1 className="welcome-text">Welcome to Papahana!</h1>
             <div className="facebook-button">
               <AccountUI />
